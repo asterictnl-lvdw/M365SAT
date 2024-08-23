9da80536ad9fe92fb37372df1d30ce42c06155ba
Leonardo van de Weteringh <leonardo.vandeweteringh@asterict.nl>
2024-08-23 16:55:59 +0200

    Made a first step adding Linux support to M365SAT in testing phase.

--- M365SATTester.ps1
+++ M365SATTester.ps1
@@ -9,21 +9,39 @@ function ExecuteM365SAT
 	Remove-Module M365SAT -Force
 }
 
-
+#The script is being designed to work with PowerShell 5.1 where there is no automatic detection of the operating system
 function CheckAdminPrivBeta
 {
 	# Check if script is running as Adminstrator and if not use RunAs
-	Write-Host "[...] Checking if the script is running as Administrator"
-	$IsAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-	if (-not $IsAdmin)
-	{
-		Write-Warning "[!] Program needs Administrator Rights! Trying to Elevate to Admin..."
-		Start-Process powershell -Verb runas -ArgumentList "-NoExit -c cd '$pwd'; .\M365SATTester.ps1"
-	}
-	else
-	{
-		Write-Host "[+] The script is running as Administrator..." -ForegroundColor Green
+	if ($OS -eq 'Windows'){
+		Write-Host "[...] Checking if the script is running as Administrator"
+		$IsAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+		if (-not $IsAdmin)
+		{
+			Write-Warning "[!] Program needs Administrator Rights! Trying to Elevate to Admin..."
+			Start-Process powershell -Verb runas -ArgumentList "-NoExit -c cd '$pwd'; .\M365SATTester.ps1"
+		}
+		else
+		{
+			Write-Host "[+] The script is running as Administrator..." -ForegroundColor Green
+			ExecuteM365SAT
+		}
+	}elseif($OS -eq 'Linux'){
+		ExecuteM365SAT
+	}elseif($OS -eq 'MacOSX'){
 		ExecuteM365SAT
 	}
+
 }
-CheckAdminPrivBeta
+
+function Get-OperatingSystem{
+	param
+	(
+		[Parameter(Mandatory = $false,
+			HelpMessage = 'Operating System: Windows / Linux / MacOSX')]
+		[ValidateSet('Windows', 'Linux', 'MacOSX', IgnoreCase = $true)]
+		[string]$OS = 'Windows'
+	)
+	CheckAdminPrivBeta
+}
+Get-OperatingSystem $args[1]
