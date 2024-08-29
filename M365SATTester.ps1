971f595743648f344ad7af26c0286f957297ac82
Leonardo van de Weteringh <leonardo.vandeweteringh@asterict.nl>
2024-08-29 12:57:11 +0200

    Added a new testcode where you can only test specific modules

--- M365SATTester.ps1
+++ M365SATTester.ps1
@@ -5,7 +5,7 @@ function ExecuteM365SAT
 	Import-Module .\M365SAT.psd1
 	#Get-M365SATReport -OutPath "C:\Out" -Username "example@example.org" -EnvironmentType AZURE,M365 -BenchmarkVersion "Latest" -Modules "All" -LicenseMode "All" -LicenseLevel "All" -reportType "HTML" -AllowLogging "Warning" -LocalMode -SkipChecks
 	Get-M365SATReport -OutPath "C:\Out" -Username "example@example.org" -EnvironmentType M365 -BenchmarkVersion "Latest" -Modules "All" -LicenseMode "E3" -LicenseLevel "All" -reportType "HTML" -AllowLogging "Warning" -LocalMode -SkipChecks
-	#Get-M365SATReport -OutPath "C:\Out" -Username "example@example.org" -EnvironmentType AZURE,M365 -BenchmarkVersion "Latest" -Modules "All" -LicenseMode "E3" -LicenseLevel "All" -reportType "HTML" -AllowLogging "Warning" -LocalMode -SkipChecks
+	#Get-M365SATReport -OutPath "C:\Out" -Username "example@example.org" -EnvironmentType M365 -BenchmarkVersion "Latest" -Modules Exchange,Azure -LicenseMode "E3" -LicenseLevel "All" -reportType "HTML" -AllowLogging "Warning" -LocalMode -SkipChecks
 	Remove-Module M365SAT -Force
 }
 
