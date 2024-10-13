199b26dc10d4a6bccb68f2a1b94e7bc44e56bbdd
Leonardo van de Weteringh <leonardo.vandeweteringh@asterict.nl>
2024-10-13 19:51:12 +0200

    Added the PowerShell PnP Automation preparation for SharePoint related Inspectors for migration purposes.

--- core/m365connectors/Connect-MicrosoftSharepoint.ps1
+++ core/m365connectors/Connect-MicrosoftSharepoint.ps1
@@ -1,3 +1,166 @@
+function Invoke-MicrosoftSharepointPnPCredentials{
+	param(
+		[string]$TenantName,
+		[System.Object]$Credential,
+		[string]$Environment
+	)
+
+	try
+	{
+		switch ($Environment) {
+			"USGovGCCHigh" 
+			{ 
+				$SpEnvironment = 'USGovernmentHigh' 
+			}
+			"USGovDoD" 
+			{ 
+				$SpEnvironment = 'USGovernmentDoD' 
+			}
+			"GermanyCloud" 
+			{ 
+				$SpEnvironment = 'Germany' 
+			}
+			"China" 
+			{ 
+				$SpEnvironment = 'China' 
+			}
+			default 
+			{ 
+				$SpEnvironment = 'Production'
+			}
+		}
+		$ClientId = Register-PnPEntraIDAppForInteractiveLogin -ApplicationName "PnP Rocks" -Tenant $TenantName.onmicrosoft.com -Credential $Credential
+		$ClientId = $ClientId.'AzureAppId/ClientId'
+		if ([string]::IsNullOrEmpty($ClientId){
+			$ClientId =  (Get-AzADApplication | Where-Object {$_.DisplayName -eq 'PnP Rocks'}).AppId
+		}
+		$Connection = Connect-PnPOnline -AzureEnvironment $Environment -Url "https://$org_name-admin.sharepoint.com" -Credential $Credential -ClientId $ClientId
+		if ((Get-PnPTenant) -ne $null)
+		{
+			Write-Host "Connected to Microsoft PnP Powershell!" -ForegroundColor DarkYellow -BackgroundColor Black
+			return $true
+		}
+		else
+		{
+			Write-ErrorLog 'Failed to Connect to Microsoft PnP Powershell' -ErrorRecord $_
+			return $false
+		}
+	}
+	catch
+	{
+		Write-ErrorLog 'Failed to Connect to Microsoft PnP Powershell' -ErrorRecord $_
+		return $false
+	}
+}
+
+function Invoke-MicrosoftSharepointPnPUsername{
+	param(
+		[string]$TenantName,
+		[string]$Environment
+	)
+
+	try
+	{
+		switch ($Environment) {
+			"USGovGCCHigh" 
+			{ 
+				$SpEnvironment = 'USGovernmentHigh' 
+			}
+			"USGovDoD" 
+			{ 
+				$SpEnvironment = 'USGovernmentDoD' 
+			}
+			"GermanyCloud" 
+			{ 
+				$SpEnvironment = 'Germany' 
+			}
+			"China" 
+			{ 
+				$SpEnvironment = 'China' 
+			}
+			default 
+			{ 
+				$SpEnvironment = 'Production'
+			}
+		}
+		$ClientId = Register-PnPEntraIDAppForInteractiveLogin -ApplicationName "PnP Rocks" -Tenant $TenantName.onmicrosoft.com -Interactive
+		$ClientId = $ClientId.'AzureAppId/ClientId'
+		if ([string]::IsNullOrEmpty($ClientId){
+			$ClientId =  (Get-AzADApplication | Where-Object {$_.DisplayName -eq 'PnP Rocks'}).AppId
+		}
+		$Connection = Connect-PnPOnline -AzureEnvironment $Environment -Url "https://$org_name-admin.sharepoint.com" -Interactive -ClientId $($ClientID.'AzureAppId/ClientId')
+		if ((Get-PnPTenant) -ne $null)
+		{
+			Write-Host "Connected to Microsoft PnP Powershell!" -ForegroundColor DarkYellow -BackgroundColor Black
+			return $true
+		}
+		else
+		{
+			Write-ErrorLog 'Failed to Connect to Microsoft PnP Powershell' -ErrorRecord $_
+			return $false
+		}
+	}
+	catch
+	{
+		Write-ErrorLog 'Failed to Connect to Microsoft PnP Powershell' -ErrorRecord $_
+		return $false
+	}
+}
+
+function Invoke-MicrosoftSharepointPnPLite{
+	param(
+		[string]$TenantName,
+		[string]$Environment
+	)
+
+	try
+	{
+		switch ($Environment) {
+			"USGovGCCHigh" 
+			{ 
+				$SpEnvironment = 'USGovernmentHigh' 
+			}
+			"USGovDoD" 
+			{ 
+				$SpEnvironment = 'USGovernmentDoD' 
+			}
+			"GermanyCloud" 
+			{ 
+				$SpEnvironment = 'Germany' 
+			}
+			"China" 
+			{ 
+				$SpEnvironment = 'China' 
+			}
+			default 
+			{ 
+				$SpEnvironment = 'Production'
+			}
+		}
+		$ClientId = Register-PnPEntraIDAppForInteractiveLogin -ApplicationName "PnP Rocks" -Tenant $TenantName.onmicrosoft.com -Interactive
+		$ClientId = $ClientId.'AzureAppId/ClientId'
+		if ([string]::IsNullOrEmpty($ClientId){
+			$ClientId =  (Get-AzADApplication | Where-Object {$_.DisplayName -eq 'PnP Rocks'}).AppId
+		}
+		$Connection = Connect-PnPOnline -AzureEnvironment $Environment -Url "https://$org_name-admin.sharepoint.com" -Interactive -ClientId $($ClientID.'AzureAppId/ClientId')
+		if ((Get-PnPTenant) -ne $null)
+		{
+			Write-Host "Connected to Microsoft PnP Powershell!" -ForegroundColor DarkYellow -BackgroundColor Black
+			return $true
+		}
+		else
+		{
+			Write-ErrorLog 'Failed to Connect to Microsoft PnP Powershell' -ErrorRecord $_
+			return $false
+		}
+	}
+	catch
+	{
+		Write-ErrorLog 'Failed to Connect to Microsoft PnP Powershell' -ErrorRecord $_
+		return $false
+	}
+}
+
 function Invoke-MicrosoftSharepointCredentials
 {
 	param(
